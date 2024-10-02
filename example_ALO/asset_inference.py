804b7cf3b67b7311c9fb720a3732ccf6422c0c2f
keewon <67584355+arc-0517@users.noreply.github.com>
2024-10-02 09:23:40 +0900

    Update asset_inference.py

---
+++ example_ALO/asset_inference.py
@@ -0,0 +1,67 @@
+#asset_[step_name].py
+ 
+# -*- coding: utf-8 -*-
+import os
+import sys
+from alolib.asset import Asset
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import pandas as pd 
+
+
+import numpy as np
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+#--------------------------------------------------------------------------------------------------------------------------
+#    CLASS
+#--------------------------------------------------------------------------------------------------------------------------
+class UserAsset(Asset):
+    def __init__(self, asset_structure):
+        super().__init__(asset_structure)
+        self.args       = self.asset.load_args()
+        self.config     = self.asset.load_config()        
+ 
+    @Asset.decorator_run
+    def run(self):
+        
+        """ data load """        
+        data = pd.read_csv(os.path.join(self.asset.get_input_path() , f"{self.args['file_name']}.csv")) # ALO API 사용
+        # data = pd.read_csv(os.path.join(self.args['data_dir'], f"{self.args['file_name']}.csv")) # custom directory 사용
+        
+        
+        """ model load """
+        model = joblib.load(os.path.join(self.asset.get_model_path(), self.args['model_file']))
+        
+        
+        """ model inference """
+        prediction = model.predict(data)
+        predict_probability = model.predict_proba(data)
+        class_probability = np.max(predict_probability, axis=1)
+        
+        data['pred_class'] = prediction
+        data['pred_proba'] = class_probability
+        
+        
+        """ result save """
+        output_path = self.asset.get_output_path() # needed: .csv only 1 / .jpg only 1 / .only, .jpg each 1
+        data.to_csv(output_path + 'output.csv')
+        
+        summary = {}
+        summary['result'] = 'OK'
+        summary['score'] = 0.98
+        summary['note'] = "The score represents the probability value of the model's prediction result."        
+        self.asset.save_summary(result=summary['result'], score=summary['score'], note=summary['note'])
+        
+        
+        """ requirement API """
+        self.asset.save_data(self.args)
+        self.asset.save_config(self.config)
+        
+ 
+ 
+#--------------------------------------------------------------------------------------------------------------------------
+#    MAIN
+#--------------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    envs, argv, data, config = {}, {}, {}, {}
+    ua = UserAsset(envs, argv, data, config)
+    ua.run()
